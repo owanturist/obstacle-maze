4b9091537255c4e53551387ab6b1e2de05af3230
Anton Ovechkin <owanturist@gmail.com>
2020-02-21 23:23:41 +0700

    Implement (de)serialisation

--- src/Maze.ts
+++ src/Maze.ts
@@ -1,5 +1,7 @@
 import Maybe, { Nothing, Just } from 'frctl/Maybe';
+import Either, { Left, Right } from 'frctl/Either';
 import Dict from 'frctl/Dict';
+
 
 /**
  * Cell's id.
@@ -172,3 +174,177 @@ export const init = (cols: number, rows: number): Maze => new MazeImpl(
     Nothing,
     Dict.empty as Dict<ID, Obstacle>
 );
+
+/**
+ * J S O N   D E C O D E  /  E N C O D E
+ */
+
+const SYMBOL_START = '*';
+const SYMBOL_TARGET = 'x';
+const SYMBOL_PATH = '.';
+const SYMBOL_WALL = '#';
+const SYMBOL_GRAVEL = '%';
+const SYMBOL_PORTAL_IN = '@';
+const SYMBOL_PORTAL_OUT = '&';
+
+const obstacleToSymbol = (obstacle: Obstacle): string => {
+    switch (obstacle) {
+        case Obstacle.Wall: {
+            return SYMBOL_WALL;
+        }
+
+        case Obstacle.Gravel: {
+            return SYMBOL_GRAVEL;
+        }
+
+        case Obstacle.PortalIn: {
+            return SYMBOL_PORTAL_IN;
+        }
+
+        case Obstacle.PortalOut: {
+            return SYMBOL_PORTAL_OUT;
+        }
+    }
+};
+
+const stepToSymbol = (step: Step): string => {
+    if (step.starting) {
+        return SYMBOL_START;
+    }
+
+    if (step.targeting) {
+        return SYMBOL_TARGET;
+    }
+
+    return step.obstacle.map(obstacleToSymbol).getOrElse(SYMBOL_PATH);
+};
+
+export const serialize = (maze: Maze): string => {
+    const { grid } = maze.fold(
+        (_id, step, acc: { grid: Array<string>; row: Array<string> }) => {
+            if (acc.row.length < maze.cols()) {
+                return {
+                    grid: acc.grid,
+                    row: [ ...acc.row, stepToSymbol(step) ]
+                };
+            }
+
+            return {
+                grid: [ ...acc.grid, acc.row.join('') ],
+                row: [ stepToSymbol(step) ]
+            };
+        },
+        { grid: [], row: [] }
+    );
+
+    return grid.join('\n');
+};
+
+type Representation = Readonly<{
+    start: Array<ID>;
+    target: Array<ID>;
+    obstacles: Dict<ID, Obstacle>;
+}>;
+
+const initialRepresentation: Representation = {
+    start: [],
+    target: [],
+    obstacles: Dict.empty as Dict<ID, Obstacle>
+};
+
+export const deserialize = (input: string): Either<string, Maze> => {
+    const rows = input.split(/\n/);
+
+    if (rows.length < 2) {
+        return Left(`It expects no less than 2 rows but got "${rows}" instead`);
+    }
+
+    const N = rows[ 0 ].length;
+
+    if (N < 2) {
+        return Left(`It expects no less than 2 cols but got "${N}" instead`);
+    }
+
+    const symbols: Array<string> = [];
+
+    for (const row of rows) {
+        if (row.length !== N) {
+            return Left(`It expects rows the same size "${N}" but got "${row.length}" instead`);
+        }
+
+        symbols.push(...row.split(''));
+    }
+
+    return symbols.reduce(
+        (acc: Either<string, Representation>, symbol, id: ID) => acc.chain(representation => {
+            switch (symbol) {
+                case SYMBOL_START: {
+                    return Right({
+                        ...representation,
+                        start: [ ...representation.start, id ]
+                    });
+                }
+
+                case SYMBOL_TARGET: {
+                    return Right({
+                        ...representation,
+                        target: [ ...representation.target, id ]
+                    });
+                }
+
+                case SYMBOL_PATH: {
+                    return Right(representation);
+                }
+
+                case SYMBOL_WALL: {
+                    return Right({
+                        ...representation,
+                        obstacles: representation.obstacles.insert(id, Obstacle.Wall)
+                    });
+                }
+
+                case SYMBOL_GRAVEL: {
+                    return Right({
+                        ...representation,
+                        obstacles: representation.obstacles.insert(id, Obstacle.Gravel)
+                    });
+                }
+
+                case SYMBOL_PORTAL_IN: {
+                    return Right({
+                        ...representation,
+                        obstacles: representation.obstacles.insert(id, Obstacle.PortalIn)
+                    });
+                }
+
+                case SYMBOL_PORTAL_OUT: {
+                    return Right({
+                        ...representation,
+                        obstacles: representation.obstacles.insert(id, Obstacle.PortalOut)
+                    });
+                }
+
+                default: {
+                    return Either.Left(`Unknown symbol ${symbol}`);
+                }
+            }
+        }),
+        Right(initialRepresentation)
+    ).chain(({ start, target, obstacles }) => {
+        if (start.length > 1) {
+            return Left('There might by only one start location');
+        }
+
+        if (target.length > 1) {
+            return Left('There might by only one target location');
+        }
+
+        return Right(new MazeImpl(
+            rows.length,
+            N,
+            Maybe.fromNullable(start[ 0 ]),
+            Maybe.fromNullable(target[ 0 ]),
+            obstacles
+        ));
+    });
+};
