c64fb08b35d5e92230cd73a9808809c09870dbd5
Anton Ovechkin <owanturist@gmail.com>
2020-02-25 15:58:06 +0700

    Update Dropzone.story

--- src/Dropzone/Dropzone.story.tsx
+++ src/Dropzone/Dropzone.story.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import * as Knobs from '@storybook/addon-knobs';
 
 import Dropzone from './index';
 
@@ -7,11 +8,16 @@ export default {
     title: 'Dropzone',
     parameters: {
         component: Dropzone
-    }
+    },
+    decorators: [ Knobs.withKnobs ]
 };
 
-export const Default = () => (
-    <Dropzone
-        onLoad={action('On Load')}
-    />
-);
+export const Default = () => {
+    const content = Knobs.text('Content', 'Choose maze filr or drag and drop');
+
+    return (
+        <Dropzone onLoad={action('On Load')}>
+            {content}
+        </Dropzone>
+    );
+};
